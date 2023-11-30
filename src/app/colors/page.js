9bf1fb298fdd78ddf14d7fec8c73713af5b72105
Ridlo Achmad Ghifary <ridloghfry@gmail.com>
2023-11-30 16:31:13 +0700

    add colors section and styling scroll-bar

---
+++ src/app/colors/page.js
@@ -0,0 +1,14 @@
+export default function Colors() {
+  return (
+    <div className="flex">
+      <div className="w-full h-[100px] bg-neutral-800">
+      </div>
+      <div className="w-full h-[100px] bg-neutral-500">
+      </div>
+      <div className="w-full h-[100px] bg-neutral-800">
+      </div>
+      <div className="w-full h-[100px] bg-neutral-500">
+      </div>
+    </div>
+  )
+}
