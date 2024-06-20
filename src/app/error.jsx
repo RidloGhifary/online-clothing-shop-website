5188d7d6e6b412ce2dd1a9bfa2759dc5bd2b213b
RidloGhifary <ridloghfry@gmail.com>
2024-06-20 15:45:56 +0700

    fix: styling error

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,5 +1,12 @@
 "use client";
 
-export default function Error(){
-  return <h1 className="text-center text-rose-500 text-3xl font-bold">Something went wrong</h1>
+export default function Error() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
+      <h1 className="text-6xl font-bold text-black md:text-8xl">500</h1>
+      <p className="text-gray-600">
+        Something went wrong, we are still fixing it...
+      </p>
+    </div>
+  );
 }
